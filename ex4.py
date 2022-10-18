1514a69a508bcbd4b9f299a22c6c437066cd7a57
chihosong <chihosong@gmail.com>
2022-10-18 14:56:32 +0900

    ..

---
+++ ex4.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+
+car = 100
+inner_space = 4.0
+driver = 30
+passenger = 90
+not_drive_car = car - driver
+drive_car = driver
+total = drive_car * inner_space
+average = passenger / drive_car
+
+print(car)
+print(driver)
+print(not_drive_car)
+print(total)
+print(passenger)
+print(average)
+print(5/2)
